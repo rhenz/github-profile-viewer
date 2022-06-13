e02d473092d5f7423afa24f165488f4590fa1c32
Renz <salvador.rhenz@gmail.com>
2022-06-14 02:26:21 +0800

    Created custom UITextfield(GPVTextField) and UIButton(GPVButton)

---
+++ Github Profile Viewer/Views/Buttons/GPVButton.swift
@@ -0,0 +1,41 @@
+//
+//  GPVButton.swift
+//  Github Profile Viewer
+//
+//  Created by John Salvador on 6/14/22.
+//
+
+import UIKit
+
+class GPVButton: UIButton {
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("You should not initialize GPVButton using init(coder:)")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        
+        configure()
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
---
+++ Github Profile Viewer/Views/Textfields/GPVTextField.swift
@@ -0,0 +1,43 @@
+//
+//  GPVTextField.swift
+//  Github Profile Viewer
+//
+//  Created by John Salvador on 6/14/22.
+//
+
+import UIKit
+
+class GPVTextField: UITextField {
+
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("You should not initialize GPVTextField using init(coder:)")
+    }
+    
+    // MARK: - Helper Methods
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        placeholder = "Enter a username"
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+    }
+}
