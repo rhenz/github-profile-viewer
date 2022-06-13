06f69b53a8276cf88d127931b78f295034068bbb
Renz <salvador.rhenz@gmail.com>
2022-06-14 00:50:39 +0800

    Created the initial FavoritesListVC and SearchVC that we are going to use soon and put them in their own folders

---
+++ Github Profile Viewer/View Controllers/Favorites List/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  Github Profile Viewer
+//
+//  Created by John Salvador on 6/14/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+}
---
+++ Github Profile Viewer/View Controllers/Search/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  Github Profile Viewer
+//
+//  Created by John Salvador on 6/14/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+}
