bd50bd8714a3219378f593e3d90800e6b24aa4e2
Renz <salvador.rhenz@gmail.com>
2022-06-16 01:49:56 +0800

    Create extension file for UIViewController to present the custom alert GPAlertVC

---
+++ Github Profile Viewer/Extensions/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  Github Profile Viewer
+//
+//  Created by John Salvador on 6/16/22.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GPVAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
