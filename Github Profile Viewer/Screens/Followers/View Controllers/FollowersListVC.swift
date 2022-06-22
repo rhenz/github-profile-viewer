8a66d39576b731b3b69fd019e10b6e7f0ad75284
Renz <salvador.rhenz@gmail.com>
2022-06-23 00:32:16 +0800

    Added UIHelper class for creating Collection View Flow Layout

--- Github Profile Viewer/Screens/Followers/View Controllers/FollowersListVC.swift
+++ Github Profile Viewer/Screens/Followers/View Controllers/FollowersListVC.swift
@@ -60,7 +60,7 @@ class FollowersListVC: UIViewController {
     }
     
     private func configureCollectionView() {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemPink
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseIdentifier)
@@ -78,20 +78,6 @@ class FollowersListVC: UIViewController {
                 self.presentGPVAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
             }
         }
-    }
-    
-    private func createThreeColumnLayout() -> UICollectionViewFlowLayout {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minimumItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-        
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-        
-        return flowLayout
     }
     
     private func configureDataSource() {
