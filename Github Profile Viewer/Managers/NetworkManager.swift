5cf3c1a9b62e3ff5e75e4c27b53b7bf57b1d5f3c
Renz <salvador.rhenz@gmail.com>
2022-07-04 09:28:11 +0800

    Implement Get User Info API to fetch specific user data from Github

--- Github Profile Viewer/Managers/NetworkManager.swift
+++ Github Profile Viewer/Managers/NetworkManager.swift
@@ -13,11 +13,9 @@ class NetworkManager {
     private let baseURL = "https://api.github.com"
     let cache = NSCache<NSString, UIImage>()
     
-    
     private init() { }
     
-    typealias GetFollowersResult = ((Result<[Follower], GPVError>) -> Void)
-    func getFollowers(for username: String, page: Int, completion: @escaping GetFollowersResult) {
+    func getFollowers(for username: String, page: Int, completion: @escaping (Result<[Follower], GPVError>) -> Void) {
         let endpoint = baseURL + "/users/\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
@@ -47,6 +45,44 @@ class NetworkManager {
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(.success(followers))
+            } catch {
+                completion(.failure(.invalidData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func getUserInfo(for username: String, completion: @escaping (Result<User, GPVError>) -> Void) {
+        let endpoint = baseURL + "/users/\(username)"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidUsername))
+            return
+        }
+        
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let _ = error {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let user = try decoder.decode(User.self, from: data)
+                completion(.success(user))
             } catch {
                 completion(.failure(.invalidData))
             }
